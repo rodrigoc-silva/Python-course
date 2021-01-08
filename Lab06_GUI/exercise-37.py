81913734096e4032c1f7c4425867ee447ef9c839
rodrigoc-silva <carvalhaes.silva.rodrigo@gmail.com>
2021-01-07 18:31:17 -0800

    Create exercise-37.py

---
+++ Lab06_GUI/exercise-37.py
@@ -0,0 +1,14 @@
+
+#empty window 1
+
+import tkinter
+
+def main():
+    #create the main window widget
+    main_window = tkinter.Tk()
+    
+    #enter the tkinter main loop
+    tkinter.mainloop()
+    
+#call the main function
+main()
